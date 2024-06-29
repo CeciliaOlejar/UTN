be824a7a444defe890a9e7b5d24088b0cbcf7127
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2024-06-28 22:03:15 -0300

    ejercicio1

---
+++ Ejercicios.js
@@ -0,0 +1,7 @@
+// ejercicio para encontrar numeros pares e impares
+let parImpar = 10;
+if (parImpar % 2 == 0) {
+    console.log("El numero es par");
+} else {
+    console.log("El numero es impar");
+}
