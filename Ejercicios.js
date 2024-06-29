a0c6a194d85e799aac18de319af7a3387787db83
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2024-06-28 22:04:44 -0300

    ejercicio5

--- Ejercicios.js
+++ Ejercicios.js
@@ -31,3 +31,10 @@ if (vacciones || diaDescanso) {
 } else {
     console.log("El padre no puede asistir al juego");
 }
+
+// ejercicio ternario
+let resultado = 3 > 2 ? "verdadero" : "falso";
+console.log(resultado);
+let numero = 9
+resultado = numero % 2 == 0 ? "par" : "impar";
+console.log(resultado);
